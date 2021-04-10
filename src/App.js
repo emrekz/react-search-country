527a80dc2c67fe52c85587505f25f9b64b96ce99
kosinos <calculatorblogg@gmail.com>
2021-04-10 15:03:55 +0300

    badge and timer added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ import { Button,
   FormControl,
   Dropdown,
   Alert,
-  Spinner
+  Spinner,
+  Card,
+  Badge
 } from 'react-bootstrap';
 
 
@@ -27,7 +29,8 @@ function App() {
   const [numberSearchResult, setNumberSearchResult] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
   const [searchStatus, setSearchStatus] = useState(false);
-
+  const [detailsBadge, setDetailsBadge] = useState(false);
+  
   useEffect(() => {
     setSearchStatus(true);
     axios.get('https://restcountries.eu/rest/v2/all')
@@ -44,7 +47,7 @@ function App() {
     if(filterState !== 'all'){
       jsonData.forEach((i) => {
         if(i[filterState].indexOf('') !== -1){
-          result.push(i)
+          result.push(i);
         }
       })
     }
@@ -55,56 +58,60 @@ function App() {
 
   let counter = 0;
   let result = [];
-  
+  let searchTimer;
   const searchFilter = (e) => {
-    setSearchStatus(true);
-    setSearchTerm(e.target.value);
-    result = [];
-    if(filterState !== 'all'){
-      jsonData.forEach((i) => {
-        if(i[filterState].indexOf(e.target.value) !== -1){
-          result.push(i);
-          setSearchStatus(false);
-        }else{setSearchStatus(false);}
-      });
-    } else {
-      jsonData.forEach((x,tm,arr) => {
-        setTimeout(() => {
-          x['details'] = [];
-          myArr = [];
-          Object.keys(x).forEach((f1) => {
-            ((typeof x[f1] === 'string' || typeof x[f1] === 'number' || x[f1] === null  || x[f1] === undefined)) ?
-              resultPush(String(x[f1]),String(e.target.value),x,String(f1),tm) :
-              Object.keys(x[f1]).forEach((f2) => {
-                ((typeof x[f1][f2] === 'string' || typeof x[f1][f2] === 'number' || x[f1][f2] === null  || x[f1][f2] === undefined)) ?
-                  resultPush(String(x[f1][f2]),String(e.target.value),x,String(f1),tm) :
-                  Object.keys(x[f1][f2]).forEach((f3) => {
-                    ((typeof x[f1][f2][f3] === 'string' || typeof x[f1][f2][f3] === 'number' || x[f1][f2][f3] === null  || x[f1][f2][f3] === undefined)) ?
-                      //console.log('key: '+f1+'>'+f2+'>'+f3+' , value: '+ x[f1][f2][f3]) :
-                      resultPush(String(x[f1][f2][f3]),String(e.target.value),x,String(f1),tm) :
-                      Object.keys(x[f1][f2][f3]).forEach((f4) => {
-                        ((typeof x[f1][f2][f3][f4] === 'string' || typeof x[f1][f2][f3][f4] === 'number' || x[f1][f2][f3][f4] === null  || x[f1][f2][f3][f4] === undefined)) ?
-                        resultPush(String(x[f1][f2][f3][f4]),String(e.target.value),x,String(f1),tm) : console.log('I am lost')
-                    })
-                  })
-              })
-          })
-          counter++;
-          if(counter === arr.length){
-            setSearchResults(result);
-            setNumberSearchResult(result.length);
-            counter = 0; 
+    clearTimeout(searchTimer);
+    searchTimer = setTimeout(() => {
+      setSearchStatus(true);
+      setSearchTerm(e.target.value);
+      result = [];
+      if(filterState !== 'all'){
+        jsonData.forEach((i) => {
+          if(i[filterState].indexOf(e.target.value) !== -1){
+            result.push(i);
             setSearchStatus(false);
-          }
-          if(myArr.length > -1){
-            x['details'] = myArr;
-          }
-        
-        }, 1)
-      })
-    }
-    setSearchResults(result);
-    setNumberSearchResult(result.length);
+          }else{setSearchStatus(false);}
+        });
+      } else {
+        setDetailsBadge(true);
+        jsonData.forEach((x,tm,arr) => {
+          setTimeout(() => {
+            x['details'] = [];
+            myArr = [];
+            Object.keys(x).forEach((f1) => {
+              ((typeof x[f1] === 'string' || typeof x[f1] === 'number' || x[f1] === null  || x[f1] === undefined)) ?
+                resultPush(String(x[f1]),String(e.target.value),x,String(f1),tm) :
+                Object.keys(x[f1]).forEach((f2) => {
+                  ((typeof x[f1][f2] === 'string' || typeof x[f1][f2] === 'number' || x[f1][f2] === null  || x[f1][f2] === undefined)) ?
+                    resultPush(String(x[f1][f2]),String(e.target.value),x,String(f1),tm) :
+                    Object.keys(x[f1][f2]).forEach((f3) => {
+                      ((typeof x[f1][f2][f3] === 'string' || typeof x[f1][f2][f3] === 'number' || x[f1][f2][f3] === null  || x[f1][f2][f3] === undefined)) ?
+                        //console.log('key: '+f1+'>'+f2+'>'+f3+' , value: '+ x[f1][f2][f3]) :
+                        resultPush(String(x[f1][f2][f3]),String(e.target.value),x,String(f1),tm) :
+                        Object.keys(x[f1][f2][f3]).forEach((f4) => {
+                          ((typeof x[f1][f2][f3][f4] === 'string' || typeof x[f1][f2][f3][f4] === 'number' || x[f1][f2][f3][f4] === null  || x[f1][f2][f3][f4] === undefined)) ?
+                          resultPush(String(x[f1][f2][f3][f4]),String(e.target.value),x,String(f1),tm) : console.log('I am lost')
+                      })
+                    })
+                })
+            })
+            counter++;
+            if(counter === arr.length){
+              setSearchResults(result);
+              setNumberSearchResult(result.length);
+              counter = 0; 
+              setSearchStatus(false);
+            }
+            if(myArr.length > -1){
+              x['details'] = myArr;
+            }
+          
+          }, 1)
+        })
+      }
+      setSearchResults(result);
+      setNumberSearchResult(result.length);
+    },600)
   };
   
   let myArr = [];
@@ -130,6 +137,8 @@ function App() {
   }
 
   const handleSelectFilter = (e) => {
+    setDetailsBadge(false);
+    document.getElementById('formControl').value = '';
     counter = 0;
     if(e.target.innerText !== filterName){
       setFilterName(e.target.innerText);
@@ -153,8 +162,8 @@ function App() {
           <Row className="justify-content-md-center">
             <Col></Col>
             <Col xs="10">
-              <Alert variant="info" >
-                { searchStatus === false ? <>Number of Results : {numberSearchResult}</> : <span style={{fontSize:'25px'}}><Spinner animation="border" variant="primary"></Spinner> Searching...</span> }
+              <Alert variant="primary">
+                { searchStatus === false ? <>Country count : <Badge variant="primary">{numberSearchResult}</Badge></> : <><Spinner animation="border" variant="primary"></Spinner><span style={{fontSize:'25px', textAlign:'center'}}> Searching...</span></> }
               </Alert>
               <Row className="justify-content-md-center">
                 <Col xs="10">
@@ -163,6 +172,7 @@ function App() {
                       <InputGroup.Text id="searchInput" style={{fontWeight:'bold'}}>{searchName}</InputGroup.Text>
                     </InputGroup.Prepend>
                     <FormControl
+                      id = "formControl"
                       placeholder="Search..."
                       aria-label="Default"
                       aria-describedby="inputGroup-sizing-default"
@@ -197,54 +207,72 @@ function App() {
                   </tr>
                 </thead>
                 <tbody>
-                  {searchResults.map((i) =>{
+                  {searchResults.map((i, ind) =>{
                     return(
                       <Fragment key={i.name}>
-                        <tr key={i.numericCode} onClick={(e) => detail(e)}>
+                        <tr key={i.numericCode} 
+                          onClick = {(e) => detail(e)}
+                        >
                           <th title='Flag'>
                             <img src={i.flag} style={{width: '3vw', height: 'auto'}} alt={i.name} />
                           </th>
                           <th title='Name'>{i.name}</th>
                           <th title='Capital'>{i.capital}</th>
                           <th title='Region'>{i.region}</th>
-                          <th><Button onClick={() => {}}>Details</Button> </th>
+                          <th><><Fragment><Button onClick={() => {}}>Details</Button></Fragment>
+                            { i.details !== undefined && detailsBadge ? <Badge variant="warning" style={{marginLeft:'5px'}}>{i.details.length}</Badge> : ''}</>
+                          </th>
                         </tr>
                         <tr className="collapse">
                           <td colSpan="12">
                               <Container>
-                              {filterState === 'all' && i.details !== undefined ?
-                                <Row style={{marginBottom:'30px'}}>
-                                  <Col style={{textAlign: 'left', color:'yellow', fontWeight:'normal'}}>                                    
-                                      <>Search results related to <b>'{searchTerm}'</b> : {Object.values(i.details).map((x,n) => <span key={x+n} style={{color:'cyan',display:'list-item',marginLeft: '2%', fontSize:'13px'}}>{x} </span> )}</>
-                                  </Col>
-                                </Row>
-                              : '' }
-                                <Row style={{fontSize:'13px'}}>
-                                  <Col xs={6} style={{textAlign:'left', display:'grid'}}>
-                                    <li><b>alpha2Code : </b>{i.alpha2Code}</li>
-                                    <li><b>alpha3Code : </b>{i.alpha3Code}</li>
-                                    <li><b>capital : </b>{i.capital}</li>
-                                    <li><b>area : </b>{i.area}</li>
-                                    <li><b>borders : </b>{ i.borders.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>
-                                    <li><b>altSpellings : </b>{ i.altSpellings.map(j => <Fragment key={j}> {j}, </Fragment>)} </li> 
-                                    <li><b>callingCodes : </b>{ i.callingCodes.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>   
-                                    <li><b>cioc : </b>{i.cioc} </li>   
-                                    <li><b>demonym : </b>{i.demonym} </li>
-                                    <li><b>languages : </b>{ i.languages.map((j,n) => <Fragment key={n}> {j.name}, </Fragment>)} </li> 
-                                    <li><b>numericCode : </b>{i.numericCode} </li>                                 
-                                  </Col>
-                                  <Col xs={6} style={{textAlign:'left', display:'grid'}}>
-                                    <li><b>subregion : </b>{i.subregion} </li>
-                                    <li><b>timezones : </b>{ i.timezones.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>
-                                    <li><b>topLevelDomain : </b>{ i.topLevelDomain.map(j => <Fragment key={j}> {j}, </Fragment>)} </li> 
-                                    <li><b>population : </b>{i.population} </li> 
-                                    <li><b>nativeName : </b>{i.nativeName} </li>  
-                                    <li><b>gini : </b>{i.gini} </li>
-                                    <li><b>latlng : </b>{i.latlng} </li>
-                                    <li><b>currencies : </b>{ i.currencies.map((j,n) => <Fragment key={n}> {j.code}, </Fragment>)} </li> 
-                                    <li><b>latlng : </b>{ i.latlng.map((j,n) => <Fragment key={n}> {j}, </Fragment>)} </li>
-                                    <li><b>regionalBlocs : </b>{ i.regionalBlocs.map((j,n) => <Fragment key={n}> {j.acronym}, </Fragment>)} </li> 
- 
+                                {filterState === 'all' && i.details !== undefined ?
+                                  <Row style={{marginBottom:'30px'}}>                                 
+                                    <Col xs={12} style={{textAlign: 'left', color:'yellow', fontWeight:'normal'}}>
+                                      <Card bg="dark" className="text-left" style={{fontSize:'15px'}}>
+                                        <Card.Header as="h6">Search results related to <b>'{searchTerm}'</b> :</Card.Header>
+                                        <Card.Body>
+                                            <span style={{fontSize:'13px',marginBottom:'5px'}}>Total result : <Badge variant="warning">{i.details.length}</Badge></span>
+                                            <>{Object.values(i.details).map((x,n) => <span key={x+n} style={{color:'cyan',display:'list-item',marginLeft: '2%', fontSize:'13px'}}>{x} </span> )}</>
+                                        </Card.Body>
+                                      </Card>
+                                    </Col>
+                                  </Row>
+                                : '' }
+                                <Row>
+                                  <Col>
+                                  <Card bg="dark" className="text-left" style={{fontSize:'13px'}}>
+                                    <Card.Header as="h6">Details</Card.Header>
+                                    <Card.Body>
+                                        <Row>
+                                        <Col xs={6} style={{textAlign:'left', display:'grid', fontSize: '12px'}}>
+                                          <li><b>alpha2Code : </b>{i.alpha2Code}</li>
+                                          <li><b>alpha3Code : </b>{i.alpha3Code}</li>
+                                          <li><b>capital : </b>{i.capital}</li>
+                                          <li><b>area : </b>{i.area}</li>
+                                          <li><b>borders : </b>{ i.borders.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>
+                                          <li><b>altSpellings : </b>{ i.altSpellings.map(j => <Fragment key={j}> {j}, </Fragment>)} </li> 
+                                          <li><b>callingCodes : </b>{ i.callingCodes.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>   
+                                          <li><b>cioc : </b>{i.cioc} </li>   
+                                          <li><b>demonym : </b>{i.demonym} </li>
+                                          <li><b>languages : </b>{ i.languages.map((j,n) => <Fragment key={n}> {j.name}, </Fragment>)} </li> 
+                                          <li><b>numericCode : </b>{i.numericCode} </li>                                 
+                                        </Col>
+                                        <Col xs={6} style={{textAlign:'left', display:'grid', fontSize: '12px'}}>
+                                          <li><b>subregion : </b>{i.subregion} </li>
+                                          <li><b>timezones : </b>{ i.timezones.map(j => <Fragment key={j}> {j}, </Fragment>)} </li>
+                                          <li><b>topLevelDomain : </b>{ i.topLevelDomain.map(j => <Fragment key={j}> {j}, </Fragment>)} </li> 
+                                          <li><b>population : </b>{i.population} </li> 
+                                          <li><b>nativeName : </b>{i.nativeName} </li>  
+                                          <li><b>gini : </b>{i.gini} </li>
+                                          <li><b>latlng : </b>{i.latlng} </li>
+                                          <li><b>currencies : </b>{ i.currencies.map((j,n) => <Fragment key={n}> {j.code}, </Fragment>)} </li> 
+                                          <li><b>latlng : </b>{ i.latlng.map((j,n) => <Fragment key={n}> {j}, </Fragment>)} </li>
+                                          <li><b>regionalBlocs : </b>{ i.regionalBlocs.map((j,n) => <Fragment key={n}> {j.acronym}, </Fragment>)} </li> 
+                                        </Col>
+                                        </Row>
+                                    </Card.Body>
+                                  </Card>
                                   </Col>
                                 </Row>
                               </Container>
